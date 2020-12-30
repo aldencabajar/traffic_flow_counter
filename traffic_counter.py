0d85725241b4dff98b8e8d2c3afff37a5c37d9d3
aldencabajar <aldenacabajar@gmail.com>
2020-12-31 03:34:47 +0800

    create function to draw boxes, establish loop through video frames

--- traffic_counter.py
+++ traffic_counter.py
@@ -8,7 +8,12 @@ import palettable
 # set parameters 
 confidence = 0.5
 threshold = 0.3
+config =  'darknet/cfg/yolov3.cfg'
+wt_file = 'data/yolov3.weights'
 video_file ='data/4K Road traffic video for object detection and tracking - free download now!.mp4'  
+out_video_file = 'annotated.'
+
+FRAME_RATE = 0.5 # there is really no need of getting each and every frame
 
 # load labels from COCO dataset
 lbl_path = 'darknet/data/coco.names'
@@ -22,16 +27,10 @@ ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
-
-# create a blob as input to the model
-print(img.shape)
-
-cap = cv2.VideoCapture(video_file)
-
-(grab, frame) = cap.read()
-
-def ForwardPassOutput(input, threshold = 0.5):
-    blob = cv2.dnn.blobFromImage(img, 1/255., (416, 416), swapRB=True, crop = False)
+def ForwardPassOutput(frame, threshold = 0.5):
+    # create a blob as input to the model
+    H, W = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1/255., (416, 416), swapRB=True, crop = False)
     net.setInput(blob)
     layerOutputs = net.forward(ln)
 
@@ -66,7 +65,74 @@ def ForwardPassOutput(input, threshold = 0.5):
                 boxes.append([int(i) for i in [x, y, width, height]])
                 class_lst.append(class_id)
                 confidences.append(float(conf))
+    #apply non maximum suppression which outputs the final predictions 
+    idx = cv2.dnn.NMSBoxes(boxes, confidences, confidence, threshold).flatten()
+    return [LABELS[class_lst[i]] for i in idx], [boxes[i] for i in idx], [confidences[i] for i in idx]
+
+def drawBoxes(frame, labels, boxes, confidences):
+    boxColor = (128, 255, 0) # very light green
+    TextColor = (255, 255, 255) # white
+    boxThickness = 3 
+    textThickness = 2
+
+    for lbl, box, conf in zip(labels, boxes, confidences):
+        start_coord = tuple(box[:2])
+        w, h = box[2:]
+        end_coord = start_coord[0] + w, start_coord[1] + h
+
+    # text to be included to the output image
+        txt = '{} ({})'.format(lbl, round(conf,3))
+        frame = cv2.rectangle(frame, start_coord, end_coord, boxColor, boxThickness)
+        frame = cv2.putText(frame, txt, start_coord, cv2.FONT_HERSHEY_SIMPLEX, 0.5, TextColor, 2)
+
+    return frame
 
 
+
+cap = cv2.VideoCapture(video_file)
+
+try:
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS)) 
+    print('Total number of frames to be processed:', num_frames,
+    '\nFrame rate (frames per second):', fps)
+except:
+    print('We cannot determine number of frames and FPS!')
+
+grab = False
+counter = 0
+
+while grab:
+    (grab, frame) = cap.read()
+    H, W = frame.shape[:2]
+    if writer is None:
+        fourccc = cv2.VideoWriter_fourcc(*'MJPG')
+        writer = cv2.VideoWriter(out_video_file, fourcc, fps, ) 
+
+
+
+    if ((counter + 1) % int(fps // FRAME_RATE):  
+        labels, boxes, confidences = ForwardPassOutput(frame) 
+        frame = drawBoxes(frame, labels, boxes, confidences) 
+
+    writer.write(frame)
+    counter += 1
+
+
+
+
+
+start = time.time()
+labels, boxes, confidences = ForwardPassOutput(frame)
+end = time.time()
+
+
+
+frame.shape
+
+
+cv2.imwrite('tmp.jpg', annotated)
+
+print('total frame processing time =', round(end - start, 3), 's')
 
 
