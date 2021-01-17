221de7d94f934503db49d1f4bfc2d9ba50f46b75
aldenacabajar <aldenacabajar@gmail.com>
2021-01-17 21:44:36 +0800

    edit setup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run app/app.py && make all
+web: sh setup.sh && streamlit run app/app.py
--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ headless=true\n\
 port=$PORT\n\
 enableCORS=false\n\
 ">~/.streamlit/config.toml
+
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
+mv yolov3-tiny.weights data/
+
+wget https://pjreddie.com/media/files/yolov3.weights
+mv yolov3.weights data/
