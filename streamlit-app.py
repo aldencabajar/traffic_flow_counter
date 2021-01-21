8fa03d923c687791f49f98282d145659af57ebb7
aldenacabajar <aldenacabajar@gmail.com>
2021-01-21 14:36:21 +0800

    initial iteration of start and stop buttons

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,6 +7,12 @@ sys.path.append(os.getcwd())
 import traffic_counter as tc
 import cv2 
 import time
+import SessionState
+from multiprocessing import Process
+from streamlit import caching
+from random import randint
+import copy
+
 
 config =  'darknet/cfg/yolov3.cfg'
 wt_file = 'data/yolov3.weights'
@@ -19,41 +25,41 @@ obj_detector = tc.ObjectDetector(wt_file, config, confidence = 0.7, nms_threshol
 
 def main():
 
+    state = SessionState.get(upload_key = None)
     hide_streamlit_widgets()
 
     st.markdown('# Vehicle Counter') 
     st.markdown('Upload a video file to track and count vehicles.')
     sidebar_options()
 
-
     upload = st.empty()
-    upload.beta_expander(label = '', expanded = True)
-
     start_button = st.empty()
     stop_button = st.empty()
 
     with upload:
-        f = st.file_uploader('Upload Video file (mpeg format)')
-
+        f = st.file_uploader('Upload Video file (mpeg format)', key = state.upload_key)
     if f is not None:
-        tfile  = tempfile.NamedTemporaryFile(delete = False)
+        tfile  = tempfile.NamedTemporaryFile(delete = True)
         tfile.write(f.read())
+
         upload.empty()
         vf = cv2.VideoCapture(tfile.name)
         start = start_button.button("start")
 
+
+        # add start and stop buttons to interupt processing
         if start:
             start_button.empty()
             stop = stop_button.button("stop")
-            
-            if stop:
-                st.stop()
-            loop_over_frames(vf)
+            start = False
+            # close out temp files
+            f.close()
+            tfile.close()
+            #update upload key
+            state.upload_key = str(randint(1000, int(1e6)))
+            print(state.upload_key)
+            loop_over_frames(vf, stop)
 
-
-
-
-        
 def hide_streamlit_widgets():
     """
     hides widgets that are displayed by streamlit when running
@@ -66,11 +72,7 @@ def hide_streamlit_widgets():
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-
-
-
-
-def loop_over_frames(vf): 
+def loop_over_frames(vf, stop): 
 
     # Main loop to process every frame and predict cars
     # get video attributes 
@@ -89,10 +91,11 @@ def loop_over_frames(vf):
     fps_meas_txt = st.empty()
     bar = st.progress(frame_counter)
     stframe = st.empty()
-
     start = time.time()
-
+    
     while vf.isOpened():
+        if stop:
+            break
         ret, frame = vf.read()
         # if frame is read correctly ret is True
         if not ret:
@@ -111,7 +114,6 @@ def loop_over_frames(vf):
 
         bar.progress(frame_counter/num_frames)
 
-
         frm = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         stframe.image(frm, width = 720)
 
@@ -119,10 +121,9 @@ def loop_over_frames(vf):
 
 def sidebar_options():
     st.sidebar.markdown('## Parameters')
-    st.sidebar.markdown('Model Confidence')
+    st.sidebar.slider('Model Confidence')
 
-if __name__ == '__main__':
-    main()
+main()
 
 
 
